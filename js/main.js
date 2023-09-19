07ee4063369a06f73d1943ba0372e560648be2ec
Tiffany Zhang <48366808+Tiffanyxk3@users.noreply.github.com>
2023-09-19 02:27:18 -0700

    Commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const themeToggle = document.getElementById("theme-toggle");
+const themeLink = document.getElementById("light-theme");
+themeToggle.addEventListener("click", () => {
+    toggleTheme();
+});
+function toggleTheme() {
+    if (themeLink.getAttribute('href') === './styles/light-style.css') {
+        themeLink.setAttribute('href', './styles/dark-style.css');
+    } else {
+        themeLink.setAttribute('href', './styles/light-style.css');
+    }
+}
